c823f857b051b71ad5017dfb032f79540b5766a3
Mufasil Iqbal <mufasil.iqbal@sourcecodetechnologies.com>
2023-09-08 14:42:17 +0500

    Bug Fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt'; // Import the JwtModule
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "src/entities/user.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: 'your-secret-key', // Replace with your actual secret key
-      signOptions: { expiresIn: '1d' }, // Adjust as needed
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "1d" },
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User]),
-    // You might also import ConfigModule here if you're using it for configuration
   ],
   controllers: [AuthController],
   providers: [AuthService],
